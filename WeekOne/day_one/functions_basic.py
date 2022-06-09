a094a5d0502b784f23045f5f9a9a3440d5cdcf9e
BenSmith2 <brsmith0224@gmail.com>
2022-06-09 15:15:11 -0400

    setting up python flask server

---
+++ WeekOne/day_one/functions_basic.py
@@ -0,0 +1,14 @@
+#1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+#2
+
+
+def number_of_military_branches():
+    return 5
+
+
+print(number_of_days_in_a_week_silicon_or_triangle_sides() +
+      number_of_military_branches())
