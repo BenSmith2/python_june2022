8371f79fa95c8da189a3d1ab5abc56b58c80e0a6
Benjamin R Smith Jr <brsmith0224@gmail.com>
2022-06-12 16:21:35 -0400

    checkerboard assingment

--- flask/fundamentals/checkerboard/checkerboard.py
+++ flask/fundamentals/checkerboard/checkerboard.py
@@ -1,12 +1,21 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+@app.route('/<int:x>/<int:y>')
+def checkerboard_3(x, y):
+    result = generate_checkerboard(x, y)
+    return render_template("index.html", result = result)
 
+def generate_checkerboard(x, y):
+    result= []
 
+    for j in range (0, y):
+        temp = []
+        for i in range(0, x):
+            temp.append((i + j) % 2)
+        result.append(temp)
 
+    return result
 
 
 if __name__=="__main__":# Run the app in debug mode.
