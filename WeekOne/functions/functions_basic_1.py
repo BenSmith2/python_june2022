352835a510a0b15d4b1ec3f5fe5e9b7a6d67097f
Benjamin R Smith Jr <brsmith0224@gmail.com>
2022-06-12 20:28:58 -0400

    functions intermediate

---
+++ WeekOne/functions/functions_basic_1.py
@@ -0,0 +1,52 @@
+# #1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+
+# #1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+
+# #3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+
+# #4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+
+# #5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+
+# #6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+
+#12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+
+#14
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
