c7701a5a147c4599bd437ad3027fe2f15af153dd
Benjamin R Smith Jr <brsmith0224@gmail.com>
2022-06-14 21:08:58 -0400

    projects

---
+++ WeekOne/fundamentals/user.py
@@ -0,0 +1,34 @@
+# make a user class
+class bank_member:
+    def __init__(self, balance, name):
+        self.balance = balance
+        self.name = name
+
+    def withdrawal(self, number):
+        self.balance -= number
+        return self
+    
+    def print_info(self):
+        print(self.balance)
+        print(self.name)
+
+    def transfer(self, number, user):
+        self.balance -= number
+        user.balance += number
+        return self
+
+
+#the user needs to have a name, account balance
+
+user1 = bank_member(100, "Ben")
+user2 = bank_member(50, "Aaron")
+
+user1.print_info()
+user1.withdrawal(10)
+print('_________________________')
+user1.print_info()
+user2.print_info()
+user1.transfer(10, user2)
+print('_________________________')
+user1.print_info()
+user2.print_info()
